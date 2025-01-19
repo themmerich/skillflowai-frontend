8402550013909337a39e132990817ffbb29b4c65
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-01-19 19:39:06 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all HTML and TypeScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
