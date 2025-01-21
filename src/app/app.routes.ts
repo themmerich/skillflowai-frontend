b520547102c1560e3e2064ce5c2b48c1ff05e532
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-01-21 20:15:51 +0100

    added first tailwind components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard',
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    title: 'Not Found',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: '404',
+  }
+];
---
+++ src/app/organisations/organisation-list/organisation-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organisation-list',
+  imports: [],
+  templateUrl: './organisation-list.component.html',
+  styleUrl: './organisation-list.component.scss'
+})
+export class OrganisationListComponent {
+
+}
