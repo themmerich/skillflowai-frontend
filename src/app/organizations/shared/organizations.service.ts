2d8bc2531659ca32a4df1c24fa9fdc75794b0b6c
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-01-30 18:29:10 +0100

    removed import

--- src/app/organizations/shared/organizations.service.ts
+++ src/app/organizations/shared/organizations.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Organization } from './organization';
 
 @Injectable({
